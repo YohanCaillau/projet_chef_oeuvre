8d6ab0f9561c5e6e21e4df32b578b8c5ba034323
Yohan Caillau <yohancaillau@live.fr>
2021-03-25 17:51:44 +0100

    add train and eval code

--- models/object_detection/code/xml_to_csv.py
+++ models/object_detection/code/xml_to_csv.py
@@ -27,9 +27,9 @@ def xml_to_csv(path):
 
 def main():
     for folder in ['train','test']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        image_path = os.path.join(os.getcwd(), ('/content/projet_chef_oeuvre/models/object_detection/data/images/' + folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        xml_df.to_csv(('/content/projet_chef_oeuvre/models/object_detection/data/annotations/' + folder + '_labels.csv'), index=None)
         print('Successfully converted xml to csv.')
 
 
